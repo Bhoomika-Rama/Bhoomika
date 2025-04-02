96076f618efab95de3f42fc901baa9877cc5402f
Bhoomika-Rama <ramakrishnareddy.b@northeastern.edu>
2025-04-02 15:18:53 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('upload').addEventListener('change', function() {
-  const files = Array.from(this.files);
-  const fileList = document.getElementById('uploaded-files');
-  fileList.innerHTML = "<strong>Uploaded Files:</strong><br>";
-  files.forEach(file => {
-    fileList.innerHTML += `${file.name}<br>`;
-  });
-});
